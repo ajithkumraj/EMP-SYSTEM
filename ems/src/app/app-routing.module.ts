497e4b75566465a810211e103b40c62b7549697a
teamphenoix <teamphenoix2022@gmail.com>
2022-03-15 16:05:44 +0530

    components created

--- ems/src/app/app-routing.module.ts
+++ ems/src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { ForgotComponent } from './auth/forgot/forgot.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NopageComponent } from './dashboard/nopage/nopage.component';
+import { ResetComponent } from './auth/reset/reset.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AttendenceComponent } from './dashboard/attendence/attendence.component';
+import { ContactComponent } from './dashboard/contact/contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { NoticeboardComponent } from './dashboard/noticeboard/noticeboard.component';
+import { SupportComponent } from './dashboard/support/support.component';
+import { TimesheetComponent } from './dashboard/timesheet/timesheet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:AuthComponent},
+  {path:'auth',component:AuthComponent,
+children:[
+  {path:'',component:LoginComponent},
+{path:'login',component:LoginComponent},
+{path:'signup',component:SignupComponent},
+{path:'reset',component:ResetComponent},
+{path:'forgot',component:ForgotComponent},]},
+{path:'dashboard',component:DashboardComponent,
+children:[{path:'',component:HomeComponent},
+{path:'home',component:HomeComponent},
+{path:'attendence',component:AttendenceComponent},
+{path:'contact',component:ContactComponent},
+{path:'nopage',component:NopageComponent},
+{path:'noticeboard',component:NoticeboardComponent},
+{path:'support',component:SupportComponent},
+{path:'timesheet',component:TimesheetComponent},
+{path:'support',component:SupportComponent},
+]},
+{path:'**',component:NopageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ems/src/app/app.module.ts
+++ ems/src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ResetComponent } from './auth/reset/reset.component';
+import { ForgotComponent } from './auth/forgot/forgot.component';
+import { NopageComponent } from './auth/nopage/nopage.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { SupportComponent } from './dashboard/support/support.component';
+import { TimesheetComponent } from './dashboard/timesheet/timesheet.component';
+import { AttendenceComponent } from './dashboard/attendence/attendence.component';
+import { NoticeboardComponent } from './dashboard/noticeboard/noticeboard.component';
+import { ContactComponent } from './dashboard/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    DashboardComponent,
+    LoginComponent,
+    SignupComponent,
+    ResetComponent,
+    ForgotComponent,
+    NopageComponent,
+    HomeComponent,
+    SupportComponent,
+    TimesheetComponent,
+    AttendenceComponent,
+    NoticeboardComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
